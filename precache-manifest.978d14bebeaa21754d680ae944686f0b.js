96dc1c9c040eddb837f381da0ab01bb8e9bb559e
Iuliia Shpak <iushp@smart-it.com>
2020-01-16 12:46:30 +0200

    Updates

---
+++ precache-manifest.978d14bebeaa21754d680ae944686f0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e863ca1da44544978ffc4382427e3039",
+    "url": "/event-list/index.html"
+  },
+  {
+    "revision": "b3b39f03681b6aac72e8",
+    "url": "/event-list/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "f7be48cc7a852aeb3cd5",
+    "url": "/event-list/static/css/main.69786d0e.chunk.css"
+  },
+  {
+    "revision": "b3b39f03681b6aac72e8",
+    "url": "/event-list/static/js/2.f381cd38.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/event-list/static/js/2.f381cd38.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7be48cc7a852aeb3cd5",
+    "url": "/event-list/static/js/main.a5576a2a.chunk.js"
+  },
+  {
+    "revision": "3da7e28aa30de9676a51",
+    "url": "/event-list/static/js/runtime-main.f2b784bb.js"
+  }
+]);
